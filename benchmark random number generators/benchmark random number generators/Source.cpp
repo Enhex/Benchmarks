d3632a85a9e430c7cc854babffb34361f6a4c6c8
Enhex <enhex0@gmail.com>
2018-02-12 11:58:19 +0200

    updated RNG benchmark

--- benchmark random number generators/benchmark random number generators/Source.cpp
+++ benchmark random number generators/benchmark random number generators/Source.cpp
@@ -7,6 +7,8 @@
 #include <boost\random\taus88.hpp>
 #include <boost\random\uniform_int_distribution.hpp>
 
+#include <pcg_random.hpp>
+
 
 ///
 /// This is the main(int argc, char** argv) for the entire celero program.
@@ -19,21 +21,49 @@ constexpr int g_iterations = 2000;
 constexpr int g_loop = 10000;
 
 
+// mt19937
 std::mt19937 rng_mt19937;
-auto random_mt19937_int = std::bind(std::uniform_int_distribution<int>(std::numeric_limits<int>::min(), std::numeric_limits<int>::max()), rng_mt19937);
-auto random_mt19937_long = std::bind(std::uniform_int_distribution<long>(std::numeric_limits<long>::min(), std::numeric_limits<long>::max()), rng_mt19937);
 
+std::uniform_int_distribution<int> distribution_int(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+std::uniform_int_distribution<long> distribution_long(std::numeric_limits<long>::min(), std::numeric_limits<long>::max());
 
-boost::random::taus88 rng_taus88;
-int random_taus88_int()
+auto random_mt19937_int() noexcept
 {
-	boost::random::uniform_int_distribution<int> dist(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
-	return dist(rng_taus88);
+	return distribution_int(rng_mt19937);
 }
-long random_taus88_long()
+
+auto random_mt19937_long() noexcept
 {
-	boost::random::uniform_int_distribution<long> dist(std::numeric_limits<long>::min(), std::numeric_limits<long>::max());
-	return dist(rng_taus88);
+	return distribution_long(rng_mt19937);
+}
+
+// taus88
+boost::random::taus88 rng_taus88;
+boost::random::uniform_int_distribution<int> dist_taus88_int(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+boost::random::uniform_int_distribution<long> dist_taus88_long(std::numeric_limits<long>::min(), std::numeric_limits<long>::max());
+
+
+int random_taus88_int() noexcept
+{
+	return dist_taus88_int(rng_taus88);
+}
+long random_taus88_long() noexcept
+{
+	return dist_taus88_long(rng_taus88);
+}
+
+
+// PCG
+pcg32 pcg;
+
+auto random_pcg_int() noexcept
+{
+	return distribution_int(pcg);
+}
+
+auto random_pcg_long() noexcept
+{
+	return distribution_long(pcg);
 }
 
 
@@ -85,47 +115,84 @@ uint64_t xorshift128plus(void) {
 }
 
 
+//#define DONT_OPTIMIIZE
+
 
 // int
 BASELINE(int, rand, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(rand());
+#else
+		rand();
+#endif
 }
 
 
 BENCHMARK(int, mt19937, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(random_mt19937_int());
+#else
+		random_mt19937_int();
+#endif
 }
 
 
 BENCHMARK(int, WELL512, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(static_cast<int>(WELLRNG512()));// using long
+#else
+		static_cast<int>(WELLRNG512());// using long
+#endif
 }
 
 
 BENCHMARK(int, taus88, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(random_taus88_int());
+#else
+		random_taus88_int();
+#endif
 }
 
 
 BENCHMARK(int, intel_fastrand, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(fastrand());
+#else
+		fastrand();
+#endif
 }
 
 
 BENCHMARK(int, xorshiftplus, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(static_cast<int>(xorshift128plus()));// using uint64_t
+#else
+		static_cast<int>(xorshift128plus());
+#endif
+}
+
+
+BENCHMARK(int, pcg, g_samples, g_iterations)
+{
+	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
+		celero::DoNotOptimizeAway(random_pcg_int());
+#else
+		random_pcg_int();
+#endif
 }
 
 
@@ -133,40 +200,75 @@ BENCHMARK(int, xorshiftplus, g_samples, g_iterations)
 BASELINE(long, rand, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
-		celero::DoNotOptimizeAway( (static_cast<long>(rand()) << (sizeof(int) * 8 - 1)) | rand() );
+#ifdef DONT_OPTIMIIZE
+		celero::DoNotOptimizeAway((static_cast<long>(rand()) << (sizeof(int) * 8 - 1)) | rand());
+#else
+		(static_cast<long>(rand()) << (sizeof(int) * 8 - 1)) | rand();
+#endif
 }
 
 
 BENCHMARK(long, mt19937, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(random_mt19937_long());
+#else
+		random_mt19937_long();
+#endif
 }
 
 
 BENCHMARK(long, WELL512, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(WELLRNG512());
+#else
+		WELLRNG512();
+#endif
 }
 
 
 BENCHMARK(long, taus88, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(random_taus88_long());
+#else
+		random_taus88_long();
+#endif
 }
 
 
 BENCHMARK(long, intel_fastrand, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway((static_cast<long>(fastrand()) << (sizeof(int) * 8 - 1)) | fastrand());
+#else
+		(static_cast<long>(fastrand()) << (sizeof(int) * 8 - 1)) | fastrand();
+#endif
 }
 
 
 BENCHMARK(long, xorshiftplus, g_samples, g_iterations)
 {
 	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
 		celero::DoNotOptimizeAway(xorshift128plus());
+#else
+		xorshift128plus();
+#endif
+}
+
+
+BENCHMARK(long, pcg, g_samples, g_iterations)
+{
+	for (size_t i = 0; i < g_loop; ++i)
+#ifdef DONT_OPTIMIIZE
+		celero::DoNotOptimizeAway(random_pcg_long());
+#else
+		random_pcg_long();
+#endif
 }
