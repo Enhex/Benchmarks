e6248bb72a982620ddf34a737c0bb4871edcd3d0
Enhex <enhex0@gmail.com>
2019-06-22 15:17:16 +0300

    exceptions: ported to conan

---
+++ exceptions/premake5.lua
@@ -0,0 +1,41 @@
+newoption {
+	trigger     = "location",
+	value       = "./",
+	description = "Where to generate the project.",
+}
+
+if not _OPTIONS["location"] then
+	_OPTIONS["location"] = "./"
+end
+
+location_dir = _OPTIONS["location"]
+
+include(location_dir .. "conanbuildinfo.premake.lua")
+
+workspace("exceptions")
+	location(location_dir)
+	conan_basic_setup()
+
+	project("exceptions")
+		kind "ConsoleApp"
+		language "C++"
+		cppdialect "C++17"
+		targetdir = location_dir .. "bin/%{cfg.buildcfg}"
+
+		files{
+			"src/**",
+		}
+
+		includedirs{
+			"src",
+		}
+
+		defines{"_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS"}
+
+		filter "configurations:Debug"
+			defines { "DEBUG" }
+			symbols "On"
+
+		filter "configurations:Release"
+			defines { "NDEBUG" }
+			optimize "On"
