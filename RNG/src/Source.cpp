7639030e9f075be8be1d45481139fec6fce0e066
Enhex <enhex0@gmail.com>
2019-06-23 22:18:10 +0300

    fixed linux build

--- RNG/src/Source.cpp
+++ RNG/src/Source.cpp
@@ -4,8 +4,8 @@
 #include <random>
 #include <limits>
 
-#include <boost\random\taus88.hpp>
-#include <boost\random\uniform_int_distribution.hpp>
+#include <boost/random/taus88.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
 
 #include <pcg_random.hpp>
 
@@ -72,22 +72,22 @@ auto random_pcg_long() noexcept
 /* initialize state to random bits */
 static unsigned long state[16];
 /* init should also reset this to 0 */
-static unsigned int index = 0;
+static unsigned int well_index = 0;
 /* return 32 bit random number */
 unsigned long WELLRNG512(void)
 {
 	unsigned long a, b, c, d;
-	a = state[index];
-	c = state[(index + 13) & 15];
+	a = state[well_index];
+	c = state[(well_index + 13) & 15];
 	b = a^c ^ (a << 16) ^ (c << 15);
-	c = state[(index + 9) & 15];
+	c = state[(well_index + 9) & 15];
 	c ^= (c >> 11);
-	a = state[index] = b^c;
+	a = state[well_index] = b^c;
 	d = a ^ ((a << 5) & 0xDA442D24UL);
-	index = (index + 15) & 15;
-	a = state[index];
-	state[index] = a^b^d ^ (a << 2) ^ (b << 18) ^ (c << 28);
-	return state[index];
+	well_index = (well_index + 15) & 15;
+	a = state[well_index];
+	state[well_index] = a^b^d ^ (a << 2) ^ (b << 18) ^ (c << 28);
+	return state[well_index];
 }
 
 
