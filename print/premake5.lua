d45ef2a19462d77a2a2f30409a9d5349d46e98f5
Enhex <enhex0@gmail.com>
2022-07-25 11:07:42 +0300

    add print benchmark

---
+++ print/premake5.lua
@@ -0,0 +1,44 @@
+newoption {
+	trigger     = "location",
+	value       = "./",
+	description = "Where to generate the project.",
+}
+
+if not _OPTIONS["location"] then
+	_OPTIONS["location"] = "./"
+end
+
+location_dir = _OPTIONS["location"]
+
+include(location_dir .. "conanbuildinfo.premake.lua")
+
+project_name = "print"
+
+workspace(project_name)
+	location(location_dir)
+	conan_basic_setup()
+
+	project(project_name)
+		kind "ConsoleApp"
+		language "C++"
+		cppdialect "C++17"
+		targetdir = location_dir .. "bin/%{cfg.buildcfg}"
+
+		files{
+			"src/**",
+		}
+
+		includedirs{
+			"src",
+		}
+
+		defines{"_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS"}
+
+		filter "configurations:Debug"
+			defines { "DEBUG" }
+			symbols "On"
+
+		filter "configurations:Release"
+			defines { "NDEBUG" }
+			optimize "On"
+			flags{"LinkTimeOptimization"}
---
+++ print/src/Source.cpp
@@ -0,0 +1,76 @@
+#include <celero/Celero.h>
+#include <fmt/format.h>
+#include <iostream>
+#include <limits>
+#include <random>
+
+CELERO_MAIN
+
+constexpr int g_samples = 50;
+constexpr int g_iterations = 3000;
+
+std::random_device rd;
+std::mt19937 rng(rd());
+auto distribution_int = std::uniform_int_distribution<>(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+auto random_int = [] {
+	return distribution_int(rng);
+};
+
+//auto distribution_float = std::uniform_real_distribution<float>(std::numeric_limits<float>::lowest(), std::numeric_limits<float>::max());
+auto distribution_float = std::uniform_real_distribution<float>(-1000000, 1000000);
+auto random_float = [] {
+	return distribution_float(rng);
+};
+
+
+BASELINE(str_lit, cout, g_samples, g_iterations)
+{
+	std::cout << "some string literal";
+}
+
+BENCHMARK(str_lit, printf, g_samples, g_iterations)
+{
+	std::printf("some string literal");
+}
+
+BENCHMARK(str_lit, fmt, g_samples, g_iterations)
+{
+	fmt::print("some string literal");
+}
+
+BENCHMARK(str_lit, puts, g_samples, g_iterations)
+{
+	std::puts("some string literal");
+}
+
+
+BASELINE(str_lit_L, cout, g_samples, g_iterations)
+{
+	std::cout << "some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+BENCHMARK(str_lit_L, printf, g_samples, g_iterations)
+{
+	std::printf("some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+}
+
+BENCHMARK(str_lit_L, fmt, g_samples, g_iterations)
+{
+	fmt::print("some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+}
+
+
+BASELINE(str_lit_LL, cout, g_samples, g_iterations)
+{
+	std::cout << "some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+}
+
+BENCHMARK(str_lit_LL, printf, g_samples, g_iterations)
+{
+	std::printf("some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ ");
+}
+
+BENCHMARK(str_lit_LL, fmt, g_samples, g_iterations)
+{
+	fmt::print("some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ some very very long string literal 1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ ");
+}
